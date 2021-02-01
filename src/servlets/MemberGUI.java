63a030d374a926d4cf34207dc02a961f9769c813
awi2d <57232634+awi2d@users.noreply.github.com>
2021-02-01 21:32:58 +0100

    Update MemberGUI.java
    added addAppointment

--- src/servlets/MemberGUI.java
+++ src/servlets/MemberGUI.java
@@ -4,14 +4,17 @@ import application.CCApplication;
 import datatypes.Appointment;
 import datatypes.PossibleDate;
 import datatypes.TimeData;
+import dbAdapters.A_Adapter;
 import interfaces.GMCmds;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MemberGUI extends HttpServlet {
 
@@ -33,6 +36,13 @@ public class MemberGUI extends HttpServlet {
                 doPost(request, response);
             }
 
+        }else if("addAppointment".equals(action)) {
+                try {
+                      request.setAttribute("pagetitle", "Add Appointment");
+                      request.getRequestDispatcher("/templates/addAppointmentGUI.ftl").forward(request, response);
+                  } catch (ServletException | IOException e) {
+                      e.printStackTrace();
+                  }
         }
     }
 
@@ -52,7 +62,7 @@ public class MemberGUI extends HttpServlet {
             int groupid = Integer.parseInt(request.getParameter("groupid"));
 
             Appointment[] appointments = new Appointment[] {
-            new Appointment("Joeys verrückte Fete", "Freibier für alle!", "online",
+            new Appointment("Joeys verrÃ¼ckte Fete", "Freibier fÃ¼r alle!", "online",
                     new TimeData(0, 0, 1, 0, 0), new String[]{},
                     new PossibleDate[]{
                             new PossibleDate(new TimeData(2021,2,28,14,30),new String[]{"Finn"})},
@@ -86,8 +96,32 @@ public class MemberGUI extends HttpServlet {
             request.setAttribute("pagetitle", "Select Date");
             int AId = Integer.parseInt(request.getParameter("AId"));
             //TODO...
-        } else if("addAppointment".equals(action)) {
-
+        }else if("addAppointmentGUISubmit".equals(action)) {
+        	request.setAttribute("pagetitle", "add Appointment pagetitle");
+        	String name = request.getParameter("Name");
+        	String descr = request.getParameter("descr");
+        	String loc = request.getParameter("loc");
+        	String date = request.getParameter("date");
+        	String plannedParticipants = request.getParameter("plannedParticipants");
+        	String posDates = request.getParameter("posDatesDate");
+        	String posDatesTime = request.getParameter("posDatesTime");
+        	String deadline = request.getParameter("deadline");
+        	String group_id = request.getParameter("group_id");
+        	TimeData dateDT = new TimeData(0, 0, 0, Integer.parseInt(date.split(":")[0]), Integer.parseInt(date.split(":")[1]));
+        	TimeData posDatesDT = new TimeData(Integer.parseInt(posDates.split("-")[0]), Integer.parseInt(posDates.split("-")[1]), Integer.parseInt(posDates.split("-")[2]), Integer.parseInt(posDates.split(":")[0]), Integer.parseInt(posDates.split(":")[1]));
+        	TimeData deadlineDT = new TimeData(Integer.parseInt(deadline.split("-")[0]), Integer.parseInt(deadline.split("-")[1]), Integer.parseInt(deadline.split("-")[2]), 0, 0);
+        	int groupid = Integer.parseInt(group_id);
+        	System.out.println("date: "+date);
+        	System.out.println("posDates: "+posDates);
+        	System.out.println("posDatesTime: "+posDatesTime);
+        	System.out.println("deadline: "+deadline);
+        	CCA.createAppointment(name, descr, loc, dateDT, new String[] {plannedParticipants}, new PossibleDate[] {new PossibleDate(posDatesDT, new String[] {plannedParticipants})}, deadlineDT,groupid);
+        	
+        	try {
+                request.getRequestDispatcher("/templates/index.ftl").forward(request, response);
+            }   catch (ServletException | IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
